d70649dcc67bf0b52d83888176985fead90dd8aa
Eef05 <157302536+Eef05@users.noreply.github.com>
2024-02-27 12:16:02 -0700

    Add files via upload

---
+++ 1a Challenge.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    int x = 3;
+     int myNum = 6;
+char myLetter = 'E';
+bool myBool = true;
+string myText = "I am working hard";
+
+ Console.WriteLine(myNum);
+Console.WriteLine(myLetter);
+Console.WriteLine(myText);
+string myText1= "did you know that 5 + 2 does not equal";
+Console.WriteLine(myText1);
+ Console.WriteLine(myNum + x);
+ Console.WriteLine(myBool);
+ int y = 7;
+ Console.WriteLine(x+=5);
+Console.WriteLine(x==y || x>y);
+Console.WriteLine(x==4);
+Console.WriteLine(x==8);
+Console.WriteLine(x/=3);
+Console.WriteLine(8/3);
+double myDoubleNum = 2.66666;
+Console.WriteLine(myDoubleNum);
+Console.WriteLine(myNum/3);
+string myText2 = "this program cannot do math with decimal places";
+Console.WriteLine(myText2);
+     }
+  }
+}
