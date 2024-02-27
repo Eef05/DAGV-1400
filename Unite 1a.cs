45c08b48e84496dcfca1053048abc11f88f7df48
Eef05 <157302536+Eef05@users.noreply.github.com>
2024-02-27 11:44:33 -0700

    Add files via upload

---
+++ Unite 1a.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    int x = 2;
+     int myNum = 5;
+char myLetter = 'D';
+bool myBool = true;
+string myText = "I am so behind";
+
+ Console.WriteLine(myNum);
+Console.WriteLine(myLetter);
+Console.WriteLine(myText);
+string myText1= "did you know that 5 + 2 equals";
+Console.WriteLine(myText1);
+ Console.WriteLine(myNum + x);
+ Console.WriteLine(myBool);
+ int y = 3;
+ Console.WriteLine(x+=3);
+Console.WriteLine(x==y || x>y);
+Console.WriteLine(x==4);
+     }
+  }
+}
